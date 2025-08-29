a6cbf6fd9a49d5135800b9f8292ee4c43fe1fd33
Henry Jaques <iphonehenry@outlook.com>
2025-08-29 12:08:19 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,61 @@
+function sortear()
+{
+    let quantidadeDeNumeros = parseInt(document.getElementById(`quantidade`).value);
+    let doNumero = parseInt(document.getElementById(`de`).value);
+    let ateONumero = parseInt(document.getElementById(`ate`).value);
+
+    let numerosSorteados = [];
+
+    for (let i = 0; i < quantidadeDeNumeros; i++)
+    {
+        let numero = sortearNumero(doNumero, ateONumero);
+
+        while(numerosSorteados.includes(numero)){
+            numero = sortearNumero(doNumero, ateONumero);
+        }
+
+        numerosSorteados.push(numero);
+    }
+
+    let resultado = document.getElementById(`resultado`);
+
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numerosSorteados}</label>`;
+
+    ativarBotao();
+
+}
+
+
+function sortearNumero(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function ativarBotao()
+{
+    let botao = document.getElementById(`btn-reiniciar`);
+    if(botao.classList.contains(`container__botao-desabilitado`))
+        {
+            botao.classList.remove(`container__botao-desabilitado`);
+            botao.classList.add(`container__botao`);
+        }
+}
+
+function desativarBotao()
+{
+    let botao = document.getElementById(`btn-reiniciar`);
+    if(botao.classList.contains(`container__botao`))
+    {
+        botao.classList.remove(`container__botao`);
+        botao.classList.add(`container__botao-desabilitado`);
+    }
+}
+
+function reiniciar()
+{
+    document.getElementById(`quantidade`).value = ``;
+    document.getElementById(`de`).value = ``;
+    document.getElementById(`ate`).value = ``;
+    document.getElementById(`resultado`).innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    desativarBotao();
+}
